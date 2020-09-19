3715f5c26886ee4c71d3b85ac13130923668f63f
mAgg-3 <68525402+mAgg-3@users.noreply.github.com>
2020-09-18 19:32:45 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var r="Mahek";
+var num=5
+
+var x= null
+var y
+function setup(){
+    console.log(r);
+    console.log(r.length);
+    console.log(r.toUpperCase());
+    console.log(num);
+    console.log(parseInt("123"));
+    console.log("a"+"b");
+    console.log(1/0);
+    console.log(2===2); 
+    console.log(x);  
+    console.log(y);
+}
